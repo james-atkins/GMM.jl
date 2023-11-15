f423deed6e66fd683940062054c719693fb9a5d5
James Atkins <code@jamesatkins.net>
2023-11-15 08:50:09 -0600

    Rearrange tests
    Split IV and OLS tests

--- test/ols.jl
+++ test/ols.jl
@@ -1,11 +1,3 @@
-import GMM:
-    gmm_num_residuals,
-    gmm_num_instruments,
-    gmm_num_parameters,
-    gmm_instruments,
-    gmm_residuals_constraints!,
-    gmm_residuals_constraints_jacobians!
-
 struct OLS <: GMMModel
     y::Vector{Float64}
     X::Matrix{Float64}
@@ -45,58 +37,6 @@ end
     y = (X * beta) .+ e
 
     model = OLS(y, X)
-
-    solve(model, I)
-end
-
-
-struct IV <: GMMModel
-    y::Vector{Float64}
-    X::Matrix{Float64}
-    Z::Matrix{Float64}
-end
-
-function gmm_num_residuals(iv::IV)
-    return size(iv.X, 1)
-end
-
-function gmm_num_instruments(iv::IV)
-    return size(iv.Z, 2)
-end
-
-function gmm_num_parameters(iv::IV)
-    return size(iv.X, 2)
-end
-
-function gmm_instruments(iv::IV)
-    return iv.Z
-end
-
-function gmm_residuals_constraints!(iv::IV, beta, residuals, constraints)
-    residuals .= iv.y .- (iv.X * beta)
-end
-
-function gmm_residuals_constraints_jacobians!(iv::IV, beta, residuals_jacobian, constraints_jacobian)
-    @. residuals_jacobian = -iv.X
-end
-
-@testset "2SLS" begin
-    beta = [0.25, 0.75]
-    pi = [0.4, 0.6]
-
-    N = 1_000
-
-    z1 = 5 .* exp.(randn(N))
-    Z = [ones(N) z1]
-    u = randn(1000)
-
-    x1 = (Z * pi) .+ u
-    X = [ones(N) x1]
-    e = randn(1000)
-
-    y = (X * beta) .+ e
-
-    model = IV(y, X, Z)
 
     solve(model, I)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
 using GMM
+import GMM:
+    gmm_num_residuals,
+    gmm_num_instruments,
+    gmm_num_parameters,
+    gmm_instruments,
+    gmm_residuals_constraints!,
+    gmm_residuals_constraints_jacobians!
 
 using Test
 import Random
@@ -7,3 +14,4 @@ using LinearAlgebra: I
 Random.seed!(12345)
 
 include("ols.jl")
+include("iv.jl")
